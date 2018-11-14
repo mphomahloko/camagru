554ca66f34f1a122c127f935d535fec1875c969d
Mpho MAHLOKO <mmahloko@e5r9p3.wethinkcode.co.za>
2018-11-14 15:06:51 +0200

    able to save an image from the canvas

--- cam.js
+++ cam.js
@@ -1,6 +1,7 @@
 var video = document.getElementById('vid1');
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var photo = document.getElementById('picture');
         
 let constraintObj = {
     audio : false,
@@ -29,7 +30,6 @@ if ( navigator.mediaDevices === undefined ) {
 }
 
 navigator.mediaDevices.getUserMedia( constraintObj ).then( function( mediaStreamObj ) {
-
  //connecting the meadia stream to the video element
  var video = document.querySelector( 'video' );
  if ( 'srcObject' in video ) {
@@ -54,5 +54,9 @@ function snap() {
     canvas.width = video.clientWidth;
     canvas.height = video.clientHeight;
     context.drawImage( video, 0, 0 );
-
+    var element = document.getElementById('picture');
+    var img = canvas.toDataURL('image/jpeg');
+    element.setAttribute('value', img);
+    // console.log(img);
+    // document.getElementById('capture-form').submit(img);
 }
--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,41 @@
-<?
+<?php
+require_once 'config/database.php';
+$path = "";
+if ( isset( $_POST['submit'] ) ) {
+    $img = $_POST['photo'];
+    $pic = explode(",",$img);
+    if ( $img ){
+        $mg = base64_decode($pic[1]);
+        $image_id = uniqid();
+        $imageDir = "./images/";
+        if ( !file_exists( $imageDir ) )
+            mkdir($imageDir);
+        file_put_contents($imageDir . $image_id . '.jpeg', $mg);
+        $path = "images/" . $image_id .".jpeg";
+        //$sql = $db->prepare("INSERT INTO
+    }
+    else {
+        echo "<script>alert ('no image');</script>";
+    }
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Camagru</title>
     </head>
     <body>
-        <video id="vid1" ></video><br>
+    <video id="vid1" ></video><br>
         <button onclick="snap();">Snap</button><br>
         <canvas id="canvas"></canvas><br>
-        <script type="text/javascript" src="./cam.js"></script>
+    <form method = "POST">
+        <input type="hidden" id = "picture" name = "photo">
+        <input type = "submit" name = "submit" value = "save image">
+    </form>
+    <script type="text/javascript" src="./cam.js"></script>
+    <?php if ( $path )
+            echo "<div><img src = '$path'></div>"; ?>
     </body>
 </html>
