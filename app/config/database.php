634c4180b3768bbe66c62742f6a8e5f1b9cecb64
Mpho <43447189+mphomahloko@users.noreply.github.com>
2021-11-20 09:40:03 +0200

    charset bugfix

--- app/config/database.php
+++ app/config/database.php
@@ -3,7 +3,7 @@ $host = $_ENV['DB_HOST'];
 $port = $_ENV['DB_PORT'];
 $username = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];
-charset = 'utf8mb4';
+$charset = 'utf8mb4';
 $db = $_ENV['DB'];
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
